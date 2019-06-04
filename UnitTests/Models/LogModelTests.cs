f68f05d114aebb48d2c7a760d357dc628c0e365d
Neha Sahu <sahuneha@seattleu.edu>
2019-06-04 11:26:56 -0700

    Fixed bug so ID does not update in LogModel, and updated unit test for coverage

--- UnitTests/Models/LogModelTests.cs
+++ UnitTests/Models/LogModelTests.cs
@@ -58,7 +58,7 @@ namespace UnitTests.Models
             // Business rule is that the ID is not updatable.  
             // So need to add a test to verify that the ID does not change
             // If the code has a bug, fix the bug and enable the test assert
-            
+
             // Arange
             var myTest = new LogModel();
             var myUpdate = new LogModel
@@ -72,9 +72,10 @@ namespace UnitTests.Models
 
             // Act
             var result = myTest.Update(myUpdate);
+           
 
             // Assert
-            //Assert.AreEqual(myTestID, myTest.ID);
+            Assert.AreEqual(myTestID, myTest.ID);
         }
     }
 }
