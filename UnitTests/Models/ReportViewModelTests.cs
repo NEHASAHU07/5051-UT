2cab33510676665b8d520893e61bb0195b6063b3
Neha Sahu <sahuneha@seattleu.edu>
2019-06-04 17:25:36 -0700

    Added ReportViewModel instantiate test case

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -8,13 +8,17 @@ namespace UnitTests.Models
     public class ReportViewModelUnitTest
     {
         [TestMethod]
+        //Instantiate test case get the 42% coverage
         public void ReportViewModel_Instantiate_Default_Should_Pass()
         {
             //Arrange
+            
 
             //Act
+            var result = new ReportViewModel();
 
             //Assert
+            Assert.IsNotNull(result);
         }
     }
 }
