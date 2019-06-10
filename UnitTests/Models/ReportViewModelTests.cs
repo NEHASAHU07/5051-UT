9e72e0f97e4e04f41ea61848bae5f650ba72c923
Neha Sahu <sahuneha@seattleu.edu>
2019-06-10 11:35:20 -0700

    Added test case tempplate for public void ReportViewModel_LogViewModel_Get_Default_Should_Pass

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -42,7 +42,16 @@ namespace UnitTests.Models
             //Assert
         }
 
+        [TestMethod]
+        public void ReportViewModel_LogViewModel_Get_Default_Should_Pass()
+        {
+            //Arrange
 
+            //act
+
+            //asset
+
+        }
 
 
 
